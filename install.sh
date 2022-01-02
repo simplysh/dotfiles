1df980004025746945ed130c07054024b76d92e9
Andrei Nemes <simply.andrei@gmail.com>
2022-01-02 21:11:10 +0000

    add firefox to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+
 # vim
 mkdir -p $HOME/.vim/autoload
 cp -fv .vim/autoload/plug.vim $HOME/.vim/autoload
 cp -fv .vimrc $HOME/.vimrc
 vim +'PlugInstall --sync' +qa
+
+exec ./elevated.sh
