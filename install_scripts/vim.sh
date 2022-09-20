7b4555817094fa017c7784c81af8bdbd18fa438d
Andrei Nemes <simply.andrei@gmail.com>
2022-09-21 00:48:15 +0100

    use gtk3

--- install_scripts/vim.sh
+++ install_scripts/vim.sh
@@ -2,15 +2,15 @@
 
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
-apt install -y libncurses-dev libgtk2.0-dev libxt-dev
+apt-get build-dep vim-gtk3 -y
 
 git -C /tmp clone --depth 1 https://github.com/vim/vim.git
 cd /tmp/vim
 
 ./configure \
   --with-features=huge \
-  --enable-gtk2-check \
-  --enable-gui=gtk2 \
+  --with-x \
+  --enable-gui=gtk3 \
   --enable-fail-if-missing
 
 make && make install
