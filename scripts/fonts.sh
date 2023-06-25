65878b8b62c2f446da961d6983f83bc9b4f4f14d
Andrei Nemes <simply.andrei@gmail.com>
2023-06-25 05:05:21 +0100

    use inconsolata on linux

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -3,8 +3,11 @@
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
 git -C /tmp clone --depth 1 https://github.com/JetBrains/JetBrainsMono.git
+git -C /tmp clone --depth 1 https://github.com/googlefonts/Inconsolata.git
 
 cp -rfv /tmp/JetBrainsMono/fonts/ttf /usr/share/fonts/truetype/jetbrains-mono
+cp -rfv /tmp/Inconsolata/fonts/ttf /usr/share/fonts/truetype/inconsolata
 
 rm -rf /tmp/JetBrainsMono
+rm -rf /tmp/Inconsolata
 
