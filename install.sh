732cb495b251f11909bf8e15eee0f13bf96f7719
Andrei Nemes <simply.andrei@gmail.com>
2024-05-10 00:51:58 +0100

    add bin install

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ bash -x ./scripts/vim.sh
 bash -x ./scripts/vimrc.sh
 bash -x ./scripts/fonts.sh
 bash -x ./scripts/firefox.sh
+bash -x ./scripts/bin.sh
---
+++ scripts/bin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gcc source/slug.c -o /usr/local/bin/slug
