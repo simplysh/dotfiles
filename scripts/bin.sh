86e3cb62e5532afdd612492f0e30975db71fddf3
Andrei Nemes <simply.andrei@gmail.com>
2025-08-20 17:59:03 +0100

    remove folders and scripts

--- scripts/bin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
-
-gcc source/slug.c -o /usr/local/bin/slug
--- scripts/tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp -fv virtual/home/.tmux.conf $HOME/.tmux.conf
-
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
-
-apt install tmux -y
